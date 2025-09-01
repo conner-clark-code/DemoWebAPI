de492a7aadf0896137d363838b6627e27e1c778e
conso <conso@VENGEANCE>
2025-09-01 16:55:41 -0700

    Add project files.

---
+++ DemoWebAPI/Controllers/AuthController.cs
@@ -0,0 +1,75 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace DemoWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        public IConfiguration Config { get; }
+
+        public AuthController(IConfiguration config)
+        {
+            Config = config;
+        }
+        public record AuthData(string? UserName, string? Password);
+        public record UserData(int Id, string UserName);
+
+        // POST: api/Auth/token
+        [HttpPost("token")]
+        public ActionResult<string> Authenticate([FromBody] AuthData loginData)
+        {
+            return ValidateCredentials(loginData);
+        }
+
+        private string GenerateToken(UserData user)
+        {
+            //Use the secret (private) key from configuration to generate the token
+            var secretKey = new SymmetricSecurityKey(
+                Encoding.ASCII.GetBytes(
+                    Config.GetValue<string>("Authentication:SecretKey") ?? throw new InvalidOperationException("Secret key not found in configuration")
+                )
+            );
+
+            //Sign the token using HMAC-SHA256 algorithm
+            var signingCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256);
+
+            //Claims
+            List<Claim> claims =
+            [
+                new(JwtRegisteredClaimNames.Sub, user.Id.ToString()),
+                new(JwtRegisteredClaimNames.UniqueName, user.UserName)
+            ];
+
+            var token = new JwtSecurityToken(
+                issuer: Config.GetValue<string>("Authentication:Issuer"),
+                audience: Config.GetValue<string>("Authentication:Audience"),
+                claims: claims,
+                notBefore: DateTime.UtcNow,
+                expires: DateTime.UtcNow.AddMinutes(1),
+                signingCredentials: signingCredentials
+            );
+        }
+
+        private UserData ValidateCredentials(AuthData userData)
+        {
+            if (userData.UserName is null || userData.Password is null)
+            {
+                throw new ArgumentException("Username or password is null");
+            }
+            // In a real application, you would validate the credentials against a user store
+            if (userData.Password != "password")
+            {
+                throw new UnauthorizedAccessException("Invalid username or password");
+            }
+
+            // Credentials are valid make token
+            return new UserData(new Random().Next(1, 1000), userData.UserName);
+        }
+    }
+}
---
+++ DemoWebAPI/Controllers/UsersController.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace DemoWebAPI.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class UsersController : ControllerBase
+{
+    public IConfiguration _config { get; }
+
+    public UsersController(IConfiguration config)
+    {
+        _config = config;
+    }
+
+    // GET: api/<UsersController>
+    [HttpGet]
+    public IEnumerable<string> Get()
+    {
+        return ["value1", "value2"];
+    }
+
+    // GET api/<UsersController>/5
+    [HttpGet("{id}")]
+    public string Get(int id)
+    {
+        return _config.GetConnectionString("DefaultConnection");
+    }
+
+    // POST: Creates a new record
+    // POST api/<UsersController>
+    [HttpPost]
+    public void Post([FromBody] string value)
+    {
+
+    }
+
+    // Updates a WHOLE record (or possibly create)
+    // PUT api/<UsersController>/5
+    [HttpPut("{id}")]
+    public void Put(int id, [FromBody] string value)
+    {
+    }
+
+    // Updates a PARTIAL record
+    // PATCH api/<UsersController>/5
+    [HttpPatch("{id}")]
+    public void Patch(int id, [FromBody] string email)
+    {
+    }
+
+    // DELETE api/<UsersController>/5
+    [HttpDelete("{id}")]
+    public void Delete(int id)
+    {
+    }
+}
